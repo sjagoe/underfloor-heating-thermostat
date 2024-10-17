cfd764c282f64240d384d5913fa003289839f4a9
Simon Jagoe <simon@simonjagoe.com>
2024-10-17 22:43:19 +0300

    Implement choosing next state

--- src/control/src/lib.rs
+++ src/control/src/lib.rs
@@ -6,12 +6,6 @@ use anyhow::Result;
 #[derive(Debug, PartialEq, PartialOrd, Clone, Copy)]
 pub struct Temperature(i16);
 
-impl Default for Temperature {
-    fn default() -> Self {
-        Temperature(0)
-    }
-}
-
 // Electricity price in cents per kWh
 // Can be negative .. that happens, sometimes
 #[derive(Debug, PartialEq, PartialOrd, Clone, Copy)]
@@ -37,13 +31,6 @@ pub enum PowerState {
     Off,
 }
 
-impl Default for PowerState {
-    fn default() -> Self {
-        PowerState::Off
-    }
-}
-
-
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub struct State {
     pub power: PowerState,
@@ -54,6 +41,22 @@ pub struct State {
 pub struct SetPoint {
     pub power: PowerState,
     pub temperature: Temperature,
+}
+
+pub fn select_temperature(
+    config: &CoreConfig,
+    current_price: ElectricityPrice,
+) -> Temperature {
+    let price_difference: f64 = (config.maximum_price.0 - current_price.0) as f64;
+
+    let scaling_factor = price_difference / config.maximum_price.0 as f64;
+
+    let temperature_range = config.maximum_temperature.0 - config.minimum_temperature.0;
+    let temperature_delta = (temperature_range as f64) * scaling_factor;
+
+    let set_temperature = config.minimum_temperature.0 + (temperature_delta as i16);
+
+    Temperature(set_temperature)
 }
 
 pub fn desired_state(
@@ -81,9 +84,10 @@ pub fn desired_state(
         return Ok(point);
     }
 
+    let set_temperature = select_temperature(config, current_price);
     let state = SetPoint {
         power: PowerState::On,
-        temperature: config.minimum_temperature,
+        temperature: set_temperature,
     };
     Ok(state)
 }
@@ -93,7 +97,46 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn test_select_temperature_zero_price() {
+        let electricity_price = ElectricityPrice(0);
+        let settings = CoreConfig {
+            minimum_temperature: Temperature(15),
+            maximum_temperature: Temperature(22),
+            turbo_temperature: Temperature(30),
+            maximum_price: ElectricityPrice(30),
+        };
+        let set_temperature = select_temperature(&settings, electricity_price);
+        assert_eq!(set_temperature, settings.maximum_temperature);
+    }
+
+    #[test]
+    fn test_select_temperature_low_price() {
+        let electricity_price = ElectricityPrice(5);
+        let settings = CoreConfig {
+            minimum_temperature: Temperature(15),
+            maximum_temperature: Temperature(22),
+            turbo_temperature: Temperature(30),
+            maximum_price: ElectricityPrice(30),
+        };
+        let set_temperature = select_temperature(&settings, electricity_price);
+        assert_eq!(set_temperature, Temperature(20));
+    }
+
+    #[test]
+    fn test_select_temperature_max_price() {
+        let electricity_price = ElectricityPrice(30);
+        let settings = CoreConfig {
+            minimum_temperature: Temperature(15),
+            maximum_temperature: Temperature(20),
+            turbo_temperature: Temperature(30),
+            maximum_price: ElectricityPrice(30),
+        };
+        let set_temperature = select_temperature(&settings, electricity_price);
+        assert_eq!(set_temperature, settings.minimum_temperature);
+    }
+
+    #[test]
+    fn test_get_desired_state_low_price() {
         let electricity_price = ElectricityPrice(5);
         let current_state = State {
             power: PowerState::On,
@@ -101,7 +144,7 @@ mod tests {
         };
         let settings = CoreConfig {
             minimum_temperature: Temperature(15),
-            maximum_temperature: Temperature(21),
+            maximum_temperature: Temperature(22),
             turbo_temperature: Temperature(30),
             maximum_price: ElectricityPrice(30),
         };
@@ -109,7 +152,51 @@ mod tests {
             desired_state(&current_state, &settings, electricity_price).expect("should work");
         let expected = SetPoint {
             power: PowerState::On,
-            temperature: Temperature(5),
+            temperature: Temperature(20),
+        };
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_get_desired_state_high_price() {
+        let electricity_price = ElectricityPrice(120);
+        let current_state = State {
+            power: PowerState::On,
+            temperature: Temperature(18),
+        };
+        let settings = CoreConfig {
+            minimum_temperature: Temperature(15),
+            maximum_temperature: Temperature(22),
+            turbo_temperature: Temperature(30),
+            maximum_price: ElectricityPrice(30),
+        };
+        let result =
+            desired_state(&current_state, &settings, electricity_price).expect("should work");
+        let expected = SetPoint {
+            power: PowerState::Off,
+            temperature: settings.minimum_temperature,
+        };
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_get_desired_state_low_temperature() {
+        let electricity_price = ElectricityPrice(120);
+        let current_state = State {
+            power: PowerState::On,
+            temperature: Temperature(12),
+        };
+        let settings = CoreConfig {
+            minimum_temperature: Temperature(15),
+            maximum_temperature: Temperature(22),
+            turbo_temperature: Temperature(30),
+            maximum_price: ElectricityPrice(30),
+        };
+        let result =
+            desired_state(&current_state, &settings, electricity_price).expect("should work");
+        let expected = SetPoint {
+            power: PowerState::On,
+            temperature: settings.turbo_temperature,
         };
         assert_eq!(result, expected);
     }
