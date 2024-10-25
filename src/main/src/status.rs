ec65f853c07f66dc3d4096b2c6a1c77d5433ad29
Simon Jagoe <simon@simonjagoe.com>
2024-10-25 10:48:05 +0300

    Restructure status event

--- src/main/src/status.rs
+++ src/main/src/status.rs
@@ -1,3 +1,20 @@
+use rgb::RGB8;
+
 mod event;
 
-pub use event::StatusEvent;
+#[derive(Debug, Clone, Copy)]
+pub enum StatusEvent {
+    Initializing,
+    Ready,
+    Collecting,
+}
+
+impl From<StatusEvent> for RGB8 {
+    fn from(status: StatusEvent) -> RGB8 {
+        match status {
+            StatusEvent::Initializing => RGB8::new(10, 10, 0),
+            StatusEvent::Ready => RGB8::new(0, 10, 0),
+            StatusEvent::Collecting => RGB8::new(0, 0, 10),
+        }
+    }
+}
--- src/main/src/status/event.rs
+++ src/main/src/status/event.rs
@@ -1,33 +1,15 @@
 use core::ffi::CStr;
 use esp_idf_svc::eventloop::*;
-use rgb::RGB8;
 
-#[derive(Debug, Clone, Copy)]
-pub enum StatusEvent {
-    Initializing,
-    Ready,
-    Collecting,
-}
-
-impl From<StatusEvent> for RGB8 {
-    fn from(status: StatusEvent) -> RGB8 {
-        match status {
-            StatusEvent::Initializing => RGB8::new(10, 10, 0),
-            StatusEvent::Ready => RGB8::new(0, 10, 0),
-            StatusEvent::Collecting => RGB8::new(0, 0, 10),
-        }
-    }
-}
-
-unsafe impl EspEventSource for StatusEvent {
+unsafe impl EspEventSource for super::StatusEvent {
     fn source() -> Option<&'static CStr> {
         // String should be unique across the whole project and ESP IDF
         Some(c"STATUS-EVENT")
     }
 }
 
-impl EspEventSerializer for StatusEvent {
-    type Data<'a> = StatusEvent;
+impl EspEventSerializer for super::StatusEvent {
+    type Data<'a> = super::StatusEvent;
 
     fn serialize<F, R>(event: &Self::Data<'_>, f: F) -> R
     where
@@ -38,11 +20,11 @@ impl EspEventSerializer for StatusEvent {
     }
 }
 
-impl EspEventDeserializer for StatusEvent {
-    type Data<'a> = StatusEvent;
+impl EspEventDeserializer for super::StatusEvent {
+    type Data<'a> = super::StatusEvent;
 
     fn deserialize<'a>(data: &EspEvent<'a>) -> Self::Data<'a> {
         // Just as easy as serializing
-        *unsafe { data.as_payload::<StatusEvent>() }
+        *unsafe { data.as_payload::<super::StatusEvent>() }
     }
 }
