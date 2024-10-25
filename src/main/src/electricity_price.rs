df4ea1e827c15d58e347357180ba4436584744be
Simon Jagoe <simon@simonjagoe.com>
2024-10-26 01:08:35 +0300

    Implement loading electricity price data from a server

--- src/main/src/electricity_price.rs
+++ src/main/src/electricity_price.rs
@@ -1,7 +1,13 @@
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use time::PrimitiveDateTime;
+use std::fmt;
+use time::{
+    ext::NumericalDuration,
+    PrimitiveDateTime,
+};
+use std::sync::{Arc, Mutex};
+use log::*;
 
 use crate::http;
 use control::ElectricityPrice;
@@ -19,10 +25,90 @@ pub struct HourlyElectricityPrice {
     pub hourly_price: HashMap<PrimitiveDateTime, ElectricityPrice>,
 }
 
-impl HourlyElectricityPrice {
-    pub fn fetch(url: &str) -> Result<HourlyElectricityPrice> {
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct MultiDayElectricityPrice {
+    today: HourlyElectricityPrice,
+    tomorrow: Option<HourlyElectricityPrice>,
+}
+
+impl MultiDayElectricityPrice {
+    fn fetch(url: &str, now: PrimitiveDateTime) -> Result<MultiDayElectricityPrice> {
         let json = http::get(url)?;
-        let data: HourlyElectricityPrice = serde_json::from_str(&json)?;
+        let data: MultiDayElectricityPrice = serde_json::from_str(&json)?;
+
+        if let Some(tomorrow) = &data.tomorrow {
+            if now >= tomorrow.valid_from && now < tomorrow.valid_until {
+                warn!("MultiDayElectricityPrice.tomorrow appears to be today's data");
+                let new_data = MultiDayElectricityPrice {
+                    today: tomorrow.clone(),
+                    tomorrow: None,
+                };
+                return Ok(new_data);
+            }
+        }
+
         Ok(data)
+    }
+}
+
+pub struct SharedElectricityPrice {
+    prices: Arc<Mutex<MultiDayElectricityPrice>>
+}
+
+impl SharedElectricityPrice {
+    pub fn fetch(url: &str, now: PrimitiveDateTime) -> Result<SharedElectricityPrice> {
+        let data = MultiDayElectricityPrice::fetch(url, now)?;
+        let shared_data = SharedElectricityPrice {
+            prices: Arc::new(Mutex::new(data)),
+        };
+        Ok(shared_data)
+    }
+
+    pub fn maybe_update(&self, url: &str) -> Result<()> {
+        let now = crate::utils::time::get_datetime()?;
+
+        let mut prices = self.prices.lock().unwrap();
+        let today = &prices.today;
+
+        warn!("now: {:?}", now);
+        warn!("valid-from {:?}", today.valid_from);
+        warn!("valid-until {:?}", today.valid_until);
+        if now >= today.valid_from && now < today.valid_until && prices.tomorrow.is_some() {
+            info!("Electricity price data is current");
+            // no need to update; we have tomorrow's data and we're still in today
+            return Ok(());
+        }
+
+        match &prices.tomorrow {
+            Some(tomorrow) => {
+                if now >= tomorrow.valid_from && now < tomorrow.valid_until {
+                    info!("Promoting tomorrow's data to being in use");
+                    prices.today = tomorrow.clone();
+                    prices.tomorrow = None;
+                }
+            }
+            None => {
+                if now > prices.today.valid_until - 3.hours() {
+                    info!("Updating electricity price data");
+                    let data = MultiDayElectricityPrice::fetch(url, now)?;
+                    prices.today = data.today;
+                    prices.tomorrow = data.tomorrow;
+                } else {
+                    info!("Current prices are valid and it's too early to fetch tomorrow");
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+
+
+impl fmt::Debug for SharedElectricityPrice {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let prices = self.prices.lock().unwrap();
+        f.debug_struct("SharedElectricityPrice")
+            .field("prices", &prices)
+            .finish()
     }
 }
